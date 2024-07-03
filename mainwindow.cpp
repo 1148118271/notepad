1a7620115ba3022e4def88bb6ed63232768fc9c8
gxk <1148118271@qq.com>
2024-07-03 22:40:36 +0800

    打开/保存文件

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,88 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QFileDialog>
+#include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    this->setCentralWidget(ui->plainTextEdit);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
+
+// 新建文件
+void MainWindow::on_new_file_triggered() {
+    qDebug() << "onNewFile";
+    this->currentFile.clear();
+    this->ui->plainTextEdit->setPlainText("");
+}
+
+// 打开文件
+void MainWindow::on_open_file_triggered() {
+    QString fileName = QFileDialog::getOpenFileName(this, "打开文件");
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QMessageBox::warning(this, "警告", "文件无法打开" + file.errorString());
+        return;
+    }
+    this->currentFile = fileName;
+    qDebug() << "file name: " << fileName;
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    this->ui->plainTextEdit->setPlainText(in.readAll());
+    file.close();
+}
+
+// 保存文件
+void MainWindow::on_save_file_triggered() {
+    if (currentFile.isEmpty()) {
+        currentFile = QFileDialog::getSaveFileName(this, "保存文件");
+    }
+    QFile file(currentFile);
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QMessageBox::warning(this, "警告", "无法保存文件:" + file.errorString());
+        return;
+    }
+    setWindowTitle(currentFile);
+    QTextStream out(&file);
+    out << this->ui->plainTextEdit->toPlainText();
+    file.close();
+}
+
+void MainWindow::on_save_as_triggered() {
+
+}
+
+
+void MainWindow::on_paste_triggered() {
+
+}
+
+
+void MainWindow::on_cut_triggered() {
+
+}
+
+
+void MainWindow::on_copy_triggered() {
+
+}
+
+
+void MainWindow::on_bolder_triggered() {
+
+}
+
+
+void MainWindow::on_italic_triggered() {
+
+}
+
+
+void MainWindow::on_underline_triggered() {
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -1,5 +1,4 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
 #include <QMainWindow>
 
@@ -9,15 +8,40 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+
+private slots:
+    void on_new_file_triggered();
+
+    void on_open_file_triggered();
+
+    void on_save_as_triggered();
+
+    void on_save_file_triggered();
+
+    void on_paste_triggered();
+
+    void on_cut_triggered();
+
+    void on_copy_triggered();
+
+    void on_bolder_triggered();
+
+    void on_italic_triggered();
+
+    void on_underline_triggered();
 
 private:
     Ui::MainWindow *ui;
+
+    QString currentFile;
+
 };
-#endif // MAINWINDOW_H
+
+
