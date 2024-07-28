3f57f594603d98006fd5565a0b0cdbe20ea8131e
gxk <1148118271@qq.com>
2024-07-28 21:35:28 +0800

    关键词高亮 普通文本

--- MyHighlighter.cpp
+++ MyHighlighter.cpp
@@ -2,7 +2,6 @@
 // Created by Administrator on 24-7-21.
 //
 
-#include <iostream>
 #include "MyHighlighter.h"
 
 MyHighlighter::MyHighlighter(QTextDocument *parent) : QSyntaxHighlighter(parent) {
@@ -12,20 +11,13 @@ MyHighlighter::MyHighlighter(QTextDocument *parent) : QSyntaxHighlighter(parent)
 void MyHighlighter::highlightBlock(const QString &text) {
     for (const HighlightRule &rule: highlightRules) {
         QRegularExpression re(rule.pattern);
-        std::cout <<"offset: " << offset << std::endl;
-        QRegularExpressionMatch rem = re.match(text);
-        if (!rem.hasMatch())
-            continue;
-        // TODO 待完善
-//        while (true) {
-//            int start = (int) rem.capturedStart();
-//            std::cout << "start: " << start << std::endl;
-//            int count = (int) rem.capturedLength();
-//            std::cout << "count: " << count << std::endl;
-//            int end = (int) rem.capturedEnd();
-//            std::cout << "end: " << end << std::endl;
-//            setFormat(start, count, rule.format);
-//        }
+        QRegularExpressionMatchIterator remi = re.globalMatch(text);
+        while (remi.hasNext()) {    
+            QRegularExpressionMatch rem = remi.next();
+            int start = (int) rem.capturedStart();
+            int count = (int) rem.capturedLength();
+            setFormat(start, count, rule.format);
+        }
     }
 
 }
